80b53352f1d95a3eed836a0dc75ea2251f60014d
Robert Starr <rstarr@indeed.com>
2023-08-05 18:16:02 -0700

    Testing adding awsconfig

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,36 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
+    "animals": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    },
     "api9813d66c": {
       "ApiId": "string",
       "ApiName": "string",
       "RootUrl": "string"
     }
   },
+  "auth": {
+    "agouracarecenterslid": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
   "function": {
     "agouracarecenterslid0dd90930": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "animalsLambda": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
