107b8b9848767bf2076d273dd1ff218c059955a8
Robert Starr <rstarr@indeed.com>
2023-08-06 21:49:51 -0700

    Added Animal Care Center info

--- src/App.js
+++ src/App.js
@@ -42,6 +42,12 @@ function App() {
                 <img alt={"Animal Care Center Logo"} src={animalCareCenter}/>
             </header>
             <PetDisplay currentPet={currentPetInfo}/>
+            <div className={"footer"}>
+                <div>Currently available for adoption at the</div>
+                <div>Agoura Animal Care Center</div>
+                <div>29525 Agoura Rd</div>
+                <div>Agoura Hills, CA 91301</div>
+            </div>
         </div>
     );
 }
