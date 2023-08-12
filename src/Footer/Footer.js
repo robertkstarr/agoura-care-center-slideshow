0d590e3399405b13213999d5a89973a59e4d41c2
Robert Starr <rstarr@indeed.com>
2023-08-12 12:32:31 -0700

    Added phone number.

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
             <div>Agoura Animal Care Center</div>
             <div>29525 Agoura Rd</div>
             <div>Agoura Hills, CA 91301</div>
+            <div>(818) 991-0071</div>
         </div>
     );
 };
