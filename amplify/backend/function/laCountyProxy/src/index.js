de84df7d2bcdf416b089d49696b507334a94d903
Robert Starr <rstarr@indeed.com>
2023-08-14 23:06:29 -0700

    Backend working

--- amplify/backend/function/laCountyProxy/src/index.js
+++ amplify/backend/function/laCountyProxy/src/index.js
@@ -1,35 +1,22 @@
+const axios = require("axios");
+const url = "https://api.lacounty.gov/accsearch/AnimalSearchServlet?pageNumber=1&pageSize=500&animalCareCenter=AGOURA&animalType=ALL&sex=ALL&breed=ALL&animalAge=ALL&animalSize=ALL&animalStatus=RTGH&animalID=&test=6";
+
 /**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ * @type {import("@types/aws-lambda").APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-    // const body = {
-    //     message: "Hello from Lambda"
-    // }
+    const response = await axios.get(url).then((response) => {
+        return response;
+    });
+
+    console.log("response received");
+    console.log(response);
+    console.log(response.data);
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "new message"}),
+        body: JSON.stringify(response.data),
         headers: {
-            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Origin": "*"
         }
-    }
-    // const response = axios.get('https://api.lacounty.gov/accsearch/AnimalSearchServlet?callback=animal&pageNumber=1&pageSize=500&animalCareCenter=AGOURA&animalType=ALL&sex=ALL&breed=ALL&animalAge=ALL&animalSize=ALL&animalStatus=RTGH&animalID=&test=6')
-    //     .then((e) => {
-    //
-    //         return {
-    //             statusCode: 200,
-    //             body: JSON.stringify({message: "test"}),
-    //             headers: {
-    //                 "Access-Control-Allow-Origin": "*",
-    //             }
-    //         };
-    //
-    //     }).catch((e) => {
-    //         return {
-    //             statusCode: 200,
-    //             body: JSON.stringify({message: e}),
-    //             headers: {
-    //                 "Access-Control-Allow-Origin": "*",
-    //             }
-    //         }
-    //     });
+    };
 };
--- src/GetPetInfo/GetPetInfo.js
+++ src/GetPetInfo/GetPetInfo.js
@@ -1,17 +1,7 @@
 import {API} from "aws-amplify";
 
 export const GetPetInfo = async () => {
-    const returnPetObject = require("../Resources/staticResponse.json");
+    const results = await API.get("backendAPI", "/animals");
 
-    API.get("backendAPI", "/animals").then((e) => console.log(e));
-
-    return returnPetObject;
-    //
-    // axios.get("https://api.lacounty.gov/accsearch/AnimalSearchServlet?callback=animal&pageNumber=1&pageSize=500&animalCareCenter=AGOURA&animalType=ALL&sex=ALL&breed=ALL&animalAge=ALL&animalSize=ALL&animalStatus=ALL&animalID=&test=4", cors({
-    //
-    //     credentials: true
-    // }), (req, res) => {
-    //     console.log(req);
-    //     console.log(res);
-    // })
+    return [results];
 };
