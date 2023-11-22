bba01df5f0d412e3cd68191c52654f8b62b8f9fa
Robert Starr <rstarr@indeed.com>
2023-11-22 13:04:45 -0800

    Added support for multiple files.

--- src/Uploader/UploaderComponent.js
+++ src/Uploader/UploaderComponent.js
@@ -6,51 +6,60 @@ import {FileUploader} from "react-drag-drop-files";
 
 const UploaderComponent = () => {
     // State to store uploaded file
-    const [file, setFile] = useState("");
+    const [files, setFiles] = useState([]);
     const fileTypes = ["JPG", "PNG", "GIF", "JPEG"];
-    // progress
+
     const [percent, setPercent] = useState(0);
 
-    // Handle file upload event and update state
-    const handleChange = (file) => {
-        setFile(file);
+    const handleChange = (newFiles) => {
+        const values = Object.keys(newFiles);
+        const newFileArray = [];
+        values.forEach((fileNumber) => {
+            newFileArray.push(newFiles[fileNumber]);
+        });
+        setFiles(oldFiles => [...oldFiles, ...newFileArray]);
     };
 
     const handleUpload = () => {
-        if (!file) {
+        if (!files) {
             alert("Please upload an image first!");
         }
 
-        const storageRef = ref(storage, `/files/${file.name}`);
-
         // progress can be paused and resumed. It also exposes progress updates.
-        // Receives the storage reference and the file to upload.
-        const uploadTask = uploadBytesResumable(storageRef, file);
+        // Receives the storage reference and the file to upload.]
+        files.forEach((file) => {
+            const storageRef = ref(storage, `/files/${file.name}`);
+            const uploadTask = uploadBytesResumable(storageRef, file);
 
-        uploadTask.on(
-            "state_changed",
-            (snapshot) => {
-                const percent = Math.round(
-                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                );
+            uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                    const percent = Math.round(
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                    );
 
-                // update progress
-                setPercent(percent);
-            },
-            (err) => console.log(err),
-            () => {
-                // download url
-                getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-                    console.log(url);
-                });
-            }
-        );
+                    // update progress
+                    setPercent(percent);
+                },
+                (err) => console.log(err),
+                () => {
+                    // download url
+                    getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+                        console.log(url);
+                    });
+                }
+            );
+        });
+
+        setFiles([]);
     };
 
     return (
         <div>
-            <FileUploader handleChange={handleChange} name="file" types={fileTypes} label={"Upload your pictures"}/>
-            <button onClick={handleUpload}>Upload to Firebase</button>
+            <FileUploader handleChange={handleChange} name="file" types={fileTypes} label={"Upload your pictures"}
+                          multiple={true}/>
+            {files.map((file, index) => <div key={index}>{index} - {file.name}</div>)}
+            <button onClick={handleUpload}>Upload Files</button>
             <p>{percent} "% done"</p>
         </div>
     );
