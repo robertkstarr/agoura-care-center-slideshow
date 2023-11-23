8afedf2694972a9a27eee7a61ed40c5128a975d6
Robert Starr <rstarr@indeed.com>
2023-11-23 10:40:07 -0800

    Moved logic to FileProgressBar

--- src/Uploader/FileProgressBar.js
+++ src/Uploader/FileProgressBar.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import "./FileProgressBar.css";
 import ProgressBar from "react-bootstrap/ProgressBar";
+import {getDownloadURL, ref, uploadBytesResumable} from "firebase/storage";
+import {storage} from "./FirebaseConfigFiles/FirebaseConfig";
 
-const FileProgressBar = ({file, index}) => {
+const FileProgressBar = ({file, index, submitting}) => {
+    const [percent, setPercent] = useState(0);
+    const handleUpload = useCallback(() => {
+        // progress can be paused and resumed. It also exposes progress updates.
+        // Receives the storage reference and the file to upload.]
+        const storageRef = ref(storage, `/files/${file.name}`);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const percent = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                );
+
+                // update progress
+                setPercent(percent);
+            },
+            (err) => console.log(err),
+            () => {
+                // download url
+                getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+                    console.log(url);
+                });
+            });
+    }, [file]);
+
+
+    useEffect(() => {
+        console.log(submitting);
+        if (submitting && percent === 0) {
+            handleUpload();
+        }
+    }, [submitting, handleUpload, percent]);
+
+
     return (
         <div key={index} className={"ProgressBar"}>
             <div className={"FileName"}>{file.name}</div>
-            <ProgressBar now={60}/>
+            <ProgressBar now={percent}/>
         </div>
     );
 };
--- src/Uploader/UploaderComponent.js
+++ src/Uploader/UploaderComponent.js
@@ -1,6 +1,4 @@
 import React, {useState} from "react";
-import {getDownloadURL, ref, uploadBytesResumable} from "firebase/storage";
-import {storage} from "./FirebaseConfigFiles/FirebaseConfig";
 import {FileUploader} from "react-drag-drop-files";
 import "./UploaderComponent.css";
 import FileProgressBar from "./FileProgressBar";
@@ -8,9 +6,8 @@ import FileProgressBar from "./FileProgressBar";
 const UploaderComponent = () => {
     // State to store uploaded file
     const [files, setFiles] = useState([]);
+    const [submitting, setSubmitting] = useState(false);
     const fileTypes = ["JPG", "PNG", "GIF", "JPEG"];
-
-    const [percent, setPercent] = useState(0);
 
     const handleChange = (newFiles) => {
         const values = Object.keys(newFiles);
@@ -21,38 +18,8 @@ const UploaderComponent = () => {
         setFiles(oldFiles => [...oldFiles, ...newFileArray]);
     };
 
-    const handleUpload = () => {
-        if (!files) {
-            alert("Please upload an image first!");
-        }
-
-        // progress can be paused and resumed. It also exposes progress updates.
-        // Receives the storage reference and the file to upload.]
-        files.forEach((file) => {
-            const storageRef = ref(storage, `/files/${file.name}`);
-            const uploadTask = uploadBytesResumable(storageRef, file);
-
-            uploadTask.on(
-                "state_changed",
-                (snapshot) => {
-                    const percent = Math.round(
-                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                    );
-
-                    // update progress
-                    setPercent(percent);
-                },
-                (err) => console.log(err),
-                () => {
-                    // download url
-                    getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-                        console.log(url);
-                    });
-                }
-            );
-        });
-
-        setFiles([]);
+    const handleSubmit = () => {
+        setSubmitting(true);
     };
 
     return (
@@ -61,13 +28,12 @@ const UploaderComponent = () => {
                           multiple={true}/>
             <div>
                 <h3>Files to upload:</h3>
-                {files.map((file, index) => <FileProgressBar file={file} index={index}/>)}
+                {files.map((file, index) => <FileProgressBar file={file} index={index} submitting={submitting}/>)}
             </div>
             <div className={"buttons"}>
-                <button onClick={handleUpload}>Submit Files</button>
+                <button onClick={handleSubmit}>Submit Files</button>
                 <button onClick={() => setFiles([])}>Clear files</button>
             </div>
-            <p>{percent}% done</p>
         </div>
     );
 };
