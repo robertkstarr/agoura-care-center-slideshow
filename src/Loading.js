76d3d3492fc2b74a863661070deb19bcd6366bb8
Robert Starr <rstarr@indeed.com>
2023-12-05 10:15:37 -0800

    Added loading icon with transparency

--- src/Loading.js
+++ src/Loading.js
@@ -1,7 +1,8 @@
 import "./Loading.css";
+import loading from "./Images/34338d26023e5515f6cc8969aa027bca.gif";
 
 const Loading = () => {
-    return <div className={"Loading"}>Loading...</div>;
+    return <div className={"Loading"}><img src={loading}/></div>;
 };
 
 export default Loading;
