626a0dfef6916ef2de1321d096ae38f9e84f231f
Robert Starr <rstarr@indeed.com>
2023-12-05 10:22:28 -0800

    Refactored loading gif

--- src/Loading.js
+++ src/Loading.js
@@ -1,5 +1,5 @@
 import "./Loading.css";
-import loading from "./Images/34338d26023e5515f6cc8969aa027bca.gif";
+import loading from "./Images/loadingDial.gif";
 
 const Loading = () => {
     return <div className={"Loading"}><img src={loading}/></div>;
