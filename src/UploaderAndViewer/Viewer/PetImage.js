0cdea5919902c22e02c46101f2fcda770e857d6f
Robert Starr <rstarr@indeed.com>
2023-12-06 12:42:30 -0800

    Added loading gif and thumbnail logic.

--- src/UploaderAndViewer/Viewer/PetImage.js
+++ src/UploaderAndViewer/Viewer/PetImage.js
@@ -8,22 +8,47 @@ import getAnimalImageURL from "./getAnimalImageURL";
 const PetImage = ({image}) => {
     const [url, setUrl] = useState("");
     const [thumbnail, setThumbnail] = useState(loadingGif);
+    const [width, setWidth] = useState(null);
+    const [height, setHeight] = useState(null);
+
+    const IMAGE_WIDTH_RATIO = .9;
 
     useEffect(() => {
         const establishUrl = async () => {
             getAnimalImageURL(image, true).then(
                 (thumbnailUrl) =>
-                    setThumbnail(thumbnailUrl ? thumbnailUrl : loadingGif)
+                    setThumbnail(thumbnailUrl)
             );
             getAnimalImageURL(image).then((animalURL) => setUrl(animalURL));
         };
 
+        if (image?.imageHeight && image?.imageWidth) {
+            const screenWidth = window.innerWidth;
+            const screenHeight = window.innerHeight;
+
+            const imageHeight = image.imageHeight;
+            const imageWidth = image.imageWidth;
+
+            if (imageHeight * screenWidth / imageWidth <= screenHeight) {
+                setHeight(imageHeight * screenWidth / imageWidth * IMAGE_WIDTH_RATIO);
+                setWidth(imageWidth * screenWidth / imageWidth * IMAGE_WIDTH_RATIO);
+            } else {
+                setHeight(imageHeight * screenHeight / imageHeight * IMAGE_WIDTH_RATIO);
+                setWidth(imageWidth * screenHeight / imageHeight * IMAGE_WIDTH_RATIO);
+            }
+        }
+
+
         establishUrl().catch(console.error);
     }, [image]);
 
-    return <div className={"PetImage"}><LazyLoadImage loading={"lazy"} src={url} alt={"animal"}
-                                                      effect={"blur"}
-                                                      placeholderSrc={thumbnail}/></div>;
+    return (
+        <div className={"PetImage"}>
+            <LazyLoadImage loading={"lazy"} src={url} alt={"animal"}
+                           effect={"blur"} width={width} height={height}
+                           placeholderSrc={thumbnail}/>
+        </div>
+    );
 };
 
 export default PetImage;
--- src/UploaderAndViewer/Viewer/getAnimalImageURL.js
+++ src/UploaderAndViewer/Viewer/getAnimalImageURL.js
@@ -1,14 +1,19 @@
 import {storage} from "../FirebaseConfigFiles/FirebaseConfig";
 import {getDownloadURL, ref} from "firebase/storage";
+import loadingGif from "../../Images/loadingDial.gif";
 
+const FILE_EXTENSION = "_200x200.";
 const getAnimalImageURLFromFilenameAndId = async (filename, animalId, thumbnail = false) => {
-    const thumbnailFolder = thumbnail ? "thumbnail/" : "";
-    return getDownloadURL(ref(storage, `${animalId}/${thumbnailFolder}${filename}`));
+    const thumbnailFolder = thumbnail ? "Thumbnails/" : "";
+    const updatedFilename = thumbnail ? filename.split(".")[0] + FILE_EXTENSION + filename.split(".")[1] : filename;
+    return getDownloadURL(ref(storage, `${animalId}/${thumbnailFolder}${updatedFilename}`));
 };
 
 const getAnimalImageURL = (image, thumbnail = false) => {
-    if (image?.fileName !== undefined && image?.animalId !== undefined) {
-        return getAnimalImageURLFromFilenameAndId(image.fileName, image.animalId);
+    if (thumbnail && (image?.fileName === undefined || image?.animalId === undefined)) {
+        return Promise.resolve(loadingGif);
+    } else if (image?.fileName !== undefined && image?.animalId !== undefined) {
+        return getAnimalImageURLFromFilenameAndId(image.fileName, image.animalId, thumbnail);
     } else if (image?.url !== undefined) {
         return Promise.resolve(image.url);
     } else {
