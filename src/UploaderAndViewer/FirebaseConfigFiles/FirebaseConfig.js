2bf494394b168c7a071cf7d3cdb252d721171176
Robert Starr <rstarr@indeed.com>
2023-12-09 19:51:37 -0800

    Removed analytics from FirebaseConfig as it was causing issues with testing

--- src/UploaderAndViewer/FirebaseConfigFiles/FirebaseConfig.js
+++ src/UploaderAndViewer/FirebaseConfigFiles/FirebaseConfig.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from "firebase/app";
-import {getAnalytics} from "firebase/analytics";
 import {getStorage} from "firebase/storage";
 import {getDatabase} from "firebase/database";
 import {getAuth} from "firebase/auth";
@@ -19,5 +18,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
 export const database = getDatabase(app);
-export const analytics = getAnalytics(app);
+// export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
