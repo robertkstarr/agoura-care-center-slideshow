e1c4079e0ce657235d971854eefba305db4a901c
Robert Starr <rstarr@indeed.com>
2023-12-17 11:18:26 -0800

    lambda function now accepting parameters

--- amplify/backend/function/laCountyProxy/src/index.js
+++ amplify/backend/function/laCountyProxy/src/index.js
@@ -1,17 +1,25 @@
 const axios = require("axios");
-const url = "https://api.lacounty.gov/accsearch/AnimalSearchServlet?pageNumber=1&pageSize=500&animalCareCenter=AGOURA&animalType=ALL&sex=ALL&breed=ALL&animalAge=ALL&animalSize=ALL&animalStatus=RTGH&animalID=&test=6";
+
+const BASE_URL = "https://api.lacounty.gov/accsearch/AnimalSearchServlet?pageNumber=1&pageSize=500&animalType=ALL&sex=ALL&breed=ALL&animalAge=ALL&animalSize=ALL";
+const AGOURA = "AGOURA";
+const READY_TO_GO_HOME = "RTGH";
 
 /**
  * @type {import("@types/aws-lambda").APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
+    let location = AGOURA;
+    let status = READY_TO_GO_HOME;
 
     if (event.multiValueQueryStringParameters) {
-        console.log(`MULTIVALUESTRINGPARAMETERS: ${JSON.stringify(event.multiValueQueryStringParameters)}`);
-        console.log(`LOCATION: ${event.multiValueQueryStringParameters.location[0]}`);
+        const params = event.multiValueQueryStringParameters;
+
+        location = params.location ? params.location[0] : AGOURA;
+        status = params.status ? params.status[0] : READY_TO_GO_HOME;
     }
-    
+
+    const url = BASE_URL + `&animalCareCenter=${location}&animalStatus=${status}`;
+
     const response = await axios.get(url).then((response) => {
         return response;
     });
