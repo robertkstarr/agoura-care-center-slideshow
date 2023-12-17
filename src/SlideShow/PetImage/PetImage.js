950c961b1ed94878cf57d4c55ed920b5a64ad68d
Robert Starr <rstarr@indeed.com>
2023-12-17 12:15:51 -0800

    Downey fully implemented

--- src/SlideShow/PetImage/PetImage.js
+++ src/SlideShow/PetImage/PetImage.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
-export const PetImage = ({imageURL, onClick}) => {
-    if (imageURL && imageURL.length > 0) {
+const PetImage = ({imageURL, onClick}) => {
+    const [error, setError] = useState(false);
+
+    if (imageURL && imageURL.length > 0 && !error) {
         return (
-            <img src={imageURL} onClick={onClick} className="Pet Image" alt="Pet"
-                 data-testid="Pet Image"/>
+            <img src={imageURL} onClick={onClick} className="PetImage" alt="Pet"
+                 data-testid="Pet Image" onError={() => {
+                setError(true);
+            }}/>
         );
+    } else if (error) {
+        return (<div className="PetImage" data-testid={"No Image"}>No Image Available.</div>);
     } else {
         return (<div>Loading...</div>);
     }
