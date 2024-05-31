fcc067caa369032940410d277eab77be71ff37ed
Robert K. Starr <robert.k.starr@gmail.com>
2024-05-31 11:27:21 -0700

    UpdatedImages: Changed Image URL. Not sure if it's enough of an improvement to justify the code change.

--- src/SlideShow/GetPetInfo/GetAnimalImageURL/GetAnimalImageURL.js
+++ src/SlideShow/GetPetInfo/GetAnimalImageURL/GetAnimalImageURL.js
@@ -1,7 +1,9 @@
 export const getAnimalImageURL = (animalId) => {
-    const BASE_URL = "https://api.lacounty.gov/data/acc/animal_images/"
-    const FILE_EXTENSION = ".jpg"
-    const petURL = BASE_URL + animalId + FILE_EXTENSION;
+    const BASE_URL = "https://animalcare.lacounty.gov/media/catalog/product/a/"
+    const FILE_EXTENSION = "_detail.jpg"
+    const subFolder = `${animalId[1]}/`
+    const petURL = BASE_URL + subFolder + animalId.toLowerCase() + FILE_EXTENSION;
 
+    console.log(petURL);
     return petURL;
 }
